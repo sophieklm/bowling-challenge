563acc5dec0656d16bac827f544c3f1fa3f6d906
Sophie Mann <sklm@live.co.uk>
2017-05-12 22:10:49 +0100

    adds initial standard rules, allows bowl and updates pins and score

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,8 +7,25 @@ describe('Game', function(){
     game = new Game();
   });
 
-  it('starts with a score of 0', function(){
-    expect(game.getScore()).toEqual(0);
-  });
+  describe('standard rules', function(){
+    it('starts with a score of 0', function(){
+      expect(game.getScore()).toEqual(0);
+    });
 
+    it('starts with 10 pins', function(){
+      expect(game.getPins()).toEqual(10);
+    });
+
+    it('reduces the number of pins when a bowl is taken', function(){
+      game.bowl(5);
+      expect(game.getPins()).toEqual(5);
+    });
+
+    it('increases the score when a bowl is taken', function(){
+      game.bowl(5);
+      expect(game.getScore()).toEqual(5);
+    });
+
+  });
+  
 });
--- src/game.js
+++ src/game.js
@@ -2,13 +2,25 @@
 
 function Game(){
   this.START_SCORE = 0;
+  this.START_PINS = 10;
   this.score = this.START_SCORE;
+  this.pins = this.START_PINS;
 }
 
 Game.prototype.getScore = function() {
   return this.score;
 };
 
-Game.prototype.setScore = function(score) {
-  return this.score = score;
+Game.prototype.setScore = function(pins) {
+  this.score += pins;
 };
+
+Game.prototype.getPins = function() {
+  return this.pins;
+};
+
+Game.prototype.bowl = function(pins) {
+  this.setScore(pins);  
+  return this.pins -= pins;
+};
+
