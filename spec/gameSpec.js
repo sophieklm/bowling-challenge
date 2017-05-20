9a43fc26b51af9a4a2cd4c83c046618aceec33a6
Sophie Mann <sklm@live.co.uk>
2017-05-20 14:46:19 +0100

    adds .travis.yml

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,73 +1,68 @@
-'use strict';
-
-describe('Game', function(){
+describe('Game', function () {
   var game;
 
-  beforeEach(function(){
+  beforeEach(function () {
     game = new Game();
   });
 
-  describe('standard rules', function(){
-    it('starts with a score of 0', function(){
+  describe('standard rules', function () {
+    it('starts with a score of 0', function () {
       expect(game.getScore()).toEqual(0);
     });
 
-    it('starts with 10 pins', function(){
+    it('starts with 10 pins', function () {
       expect(game.getPins()).toEqual(10);
     });
 
-    it('reduces the number of pins when a bowl is taken', function(){
+    it('reduces the number of pins when a bowl is taken', function () {
       game.bowl(5);
       expect(game.getPins()).toEqual(5);
     });
 
-    it('increases the score when a bowl is taken', function(){
+    it('increases the score when a bowl is taken', function () {
       game.bowl(5);
       expect(game.getScore()).toEqual(5);
     });
 
-    it('knows which frame it is in', function(){
+    it('knows which frame it is in', function () {
       expect(game.getFrame()).toEqual(1);
     });
 
-    it('knows how many bowls have been played', function(){
+    it('knows how many bowls have been played', function () {
       expect(game.getBowl()).toEqual(1);
     });
 
-    it('has two bowls per frame', function(){
+    it('has two bowls per frame', function () {
       game.bowl(1);
       expect(game.getBowl()).toEqual(2);
     });
 
-    it('increments the frame after two bowls', function(){
+    it('increments the frame after two bowls', function () {
       game.bowl(1);
       game.bowl(1);
       expect(game.getFrame()).toEqual(2);
     });
 
-    it('resets the pins after two bowls', function(){
+    it('resets the pins after two bowls', function () {
       game.bowl(1);
       game.bowl(1);
       expect(game.getPins()).toEqual(10);
     });
 
-    it('can only have ten frames', function(){
-      for(var i=0 ; i<20; i++) {
+    it('can only have ten frames', function () {
+      for(var i = 0; i < 20; i++) {
         game.bowl(1);
-      };
-      expect(game.getFrame()).toEqual(10);
-      expect(function(){game.bowl(1)}).toThrowError('Game Over!');
+      }
+      expect(function () { game.bowl(1) }).toThrowError('Game Over!');
     });
 
-    it('incrememnts frame after strike', function(){
+    it('incrememnts frame after strike', function () {
       game.bowl(10);
       expect(game.getFrame()).toEqual(2);
-    })
+    });
   });
 
-  describe('last frame', function(){
-
+  describe('last frame', function () {
 
   });
-  
 });
--- src/game.js
+++ src/game.js
@@ -1,59 +1,63 @@
-'use strict';
-
-function Game(){
+function Game() {
   this.START_SCORE = 0;
   this.START_PINS = 10;
+  this.MAX_FRAMES = 10;
   this.score = this.START_SCORE;
   this.pins = this.START_PINS;
   this.frame = 1;
   this.framebowl = 1;
 }
 
-Game.prototype.getScore = function() {
+Game.prototype.getScore = function () {
   return this.score;
 };
 
-Game.prototype.setScore = function(pins) {
+Game.prototype.setScore = function (pins) {
   this.score += pins;
 };
 
-Game.prototype.getPins = function() {
+Game.prototype.getPins = function () {
   return this.pins;
 };
 
-Game.prototype.setPins = function(pins){
-  return this.pins -= pins;
+Game.prototype.setPins = function (pins) {
+  this.pins -= pins;
 };
 
-Game.prototype.resetPins = function(){
+Game.prototype.resetPins = function () {
   this.pins = this.START_PINS;
 };
 
-Game.prototype.getFrame = function(){
+Game.prototype.getFrame = function () {
   return this.frame;
 };
 
-Game.prototype.incrementFrame = function(set){
+Game.prototype.incrementFrame = function (set) {
   this.frame += set;
 };
 
-Game.prototype.getBowl = function(){
+Game.prototype.getBowl = function () {
   return this.framebowl;
 };
 
-Game.prototype.changeBowl = function(set){
+Game.prototype.changeBowl = function (set) {
   this.framebowl = set;
 };
 
-Game.prototype.bowl = function(pins) {
-  if(this.getBowl() === 1){
+Game.prototype.isStrike = function () {
+
+};
+
+Game.prototype.bowl = function (pins) {
+  if (this.getFrame() > this.MAX_FRAMES) {
+    throw new Error('Game Over!');
+  } if (this.getBowl() === 1) {
     this.setPins(pins);
     this.changeBowl(2);
-  }
-  else if(this.getBowl() === 2){
+  } else if (this.getBowl() === 2) {
     this.incrementFrame(1);
     this.resetPins();
     this.changeBowl(1);
   }
-  this.setScore(pins);  
+  this.setScore(pins);
 };
